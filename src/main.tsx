451926ed603be14dc2bffa56b0ae08be1b5320d3
YuraKolesnikov <jurijs_kolesnikovs@inbox.lv>
2024-04-30 10:45:19 +0700

    Styles setup

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import * as ReactDOM from 'react-dom/client'
 
 import App from './app.tsx'
 
-import './vars.pcss'
+import './styles/index.pcss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,21 +1,7 @@
-import { observer } from 'mobx-react-lite'
-import userStore from 'store'
-import { getTypeOf } from 'lib/get-type-of/get-type-of'
-
-export const Home = observer(() => {
-  const promise = new Promise((resolve) => {
-    resolve()
-  })
-  console.log(getTypeOf(promise))
+export const Home = () => {
   return (
     <section>
-      <button onClick={() => userStore.updateName('Yura', 'Kolesnikov')}>
-        Yura Kolesnikov
-      </button>
-      <button onClick={() => userStore.updateName('Yana', 'Kolesnikova')}>
-        Yana Kolesnikova
-      </button>
-      <p>{userStore.fullName}</p>
+      Home
     </section>
   )
-})
+}
