47188abe739747a64c5672420100642f0b1994d7
YuraKolesnikov <jurijs_kolesnikovs@inbox.lv>
2024-05-11 09:04:47 +0700

    vite config small update and tab fix in vars.pcss

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import eslint from 'vite-plugin-eslint'
 
+const getPath = (folder: string): string => path.resolve(__dirname, `src/${folder}`)
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [eslint(), react()],
@@ -14,11 +16,11 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      'components': path.resolve(__dirname, 'src/components'),
-      'lib': path.resolve(__dirname, 'src/lib'),
-      'pages': path.resolve(__dirname, 'src/pages'),
-      'store': path.resolve(__dirname, 'src/store'),
-      'types': path.resolve(__dirname, 'src/types'),
+      'pages':      path.resolve(__dirname, getPath('pages')),
+      'components': path.resolve(__dirname, getPath('components')),
+      'store':      path.resolve(__dirname, getPath('store')),
+      'lib':        path.resolve(__dirname, getPath('lib')),
+      'types':      path.resolve(__dirname, getPath('types')),
     }
   },
 })
