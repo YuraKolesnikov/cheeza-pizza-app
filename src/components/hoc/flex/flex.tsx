0ad1968448b08f880d18a6a850d0e12cec085511
j.kolesnikovs <j.kolesnikovs@vk.team>
2025-10-18 10:08:32 +0300

    Flex updates

--- src/components/hoc/flex/flex.tsx
+++ src/components/hoc/flex/flex.tsx
@@ -5,41 +5,47 @@ import { IFlexProps } from './types'
 import styles from './index.module.pcss'
 
 export const Flex = ({
-  align = 'center',
-  justify = 'center',
-  gap = 0,
-  tag = 'div',
-  className,
-  onClick,
-  htmlFor,
-  children,
+	align = 'center',
+	justify = 'center',
+	gap = 0,
+	tag = 'div',
+	className,
+	onClick,
+	htmlFor,
+	children,
 }: IFlexProps) => {
-  const classList = cn(
-    styles['flex'],
-    styles[`align--${align}`],
-    styles[`justify--${justify}`],
-    className
-  )
+	const classList = cn(
+		styles['flex'],
+		styles[`align--${align}`],
+		styles[`justify--${justify}`],
+		className
+	)
 
-  const flexProps = {
-    className: classList,
-    style: {
-      gap,
-    },
-    ...(tag === 'label' && {
-      htmlFor,
-    }),
-  }
+	const flexProps = {
+		className: classList,
+		style: {
+			gap,
+		},
+		...(tag === 'label' && {
+			htmlFor,
+		}),
+	}
 
-  return tag === 'div' ? (
-    <div onClick={onClick} {...flexProps}>
-      {children}
-    </div>
-  ) : tag === 'label' ? (
-    <label {...flexProps}>{children}</label>
-  ) : (
-    <button onClick={onClick} {...flexProps}>
-      {children}
-    </button>
-  )
+	if (tag === 'button') {
+		return (
+			<button onClick={onClick} {...flexProps}>
+				{children}
+			</button>
+		)
+	}
+
+	if (tag === 'label') {
+		return <label {...flexProps}>{children}</label>
+	}
+
+	return (
+		<div onClick={onClick} {...flexProps}>
+			{children}
+		</div>
+	)
 }
--- src/components/hoc/flex/types.ts
+++ src/components/hoc/flex/types.ts
@@ -1,12 +1,12 @@
 import { MouseEventHandler } from 'react'
 
 export interface IFlexProps {
-  align: 'center' | 'start' | 'end' | 'stretch'
-  justify: 'center' | 'start' | 'between' | 'around' | 'evenly'
-  gap: number
-  tag: 'div' | 'button' | 'label'
-  className?: string
-  onClick?: MouseEventHandler<HTMLDivElement | HTMLButtonElement>
-  htmlFor?: string
-  children: ReactNode
+	align: 'center' | 'start' | 'end' | 'stretch'
+	justify: 'center' | 'start' | 'between' | 'around' | 'evenly'
+	gap?: number
+	tag?: 'div' | 'button' | 'label'
+	className?: string
+	onClick?: MouseEventHandler<HTMLDivElement | HTMLButtonElement>
+	htmlFor?: string
+	children: ReactNode
 }
